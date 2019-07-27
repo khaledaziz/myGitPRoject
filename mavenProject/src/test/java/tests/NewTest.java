c4c5e7b33f610bf12d5c6b4262dc0cf950af4dec
khale <khale@DESKTOP-GN1IAVO>
2019-07-27 23:50:30 +0200

    test commit

---
+++ mavenProject/src/test/java/tests/NewTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	
+	public WebDriver driver;
+	
+  @Test
+  public void openMyBlog() {
+	driver.get("https://www.softwaretestingmaterial.com/");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  
+	  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//drivers//chromedriver (2).exe");
+		//create chrome instance
+		driver = new ChromeDriver();
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
---
+++ mavenProject/src/test/java/tests/NewTestTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class NewTestTest {
+
+  @Test
+  public void afterClass() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void beforeClass() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void openMyBlog() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
